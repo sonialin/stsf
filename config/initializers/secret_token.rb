7e744c9906117985af5098a1d347d04452c2a548
Sonia Lin <sonia_lin@hotmail.com>
2013-12-22 23:32:06 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stsf::Application.config.secret_key_base = '19052edc065cfe16c56bad16e4f20fd019c9eb42eb5b38dfff8779ed23603b3d817d3cb49be72d063bec7e53227901f96072e370538dc7b501d732a15baecaf2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stsf::Application.config.session_store :cookie_store, key: '_stsf_session'
